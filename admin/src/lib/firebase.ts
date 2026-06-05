c71844c994f6b673eff4a5b1e0104586bd30c33c
Tom Arra <tarra3@gmail.com>
2026-06-05 10:52:43 -0500

    spelling

--- admin/src/lib/firebase.ts
+++ admin/src/lib/firebase.ts
@@ -1,30 +1,30 @@
-// cspell:ignore DXCUM VDBJ appspot DKWD FGUK
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getFunctions } from 'firebase/functions';
+// cspell:ignore DXCUM VDBJ DKWD FGUK
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getFunctions } from "firebase/functions";
 
 const devConfig = {
-  apiKey: 'AIzaSyDXCUM2AxKD73rQIy53ArxDspVYbVDBJ68',
-  authDomain: 'curling-scoreboard-dev.firebaseapp.com',
-  projectId: 'curling-scoreboard-dev',
-  storageBucket: 'curling-scoreboard-dev.appspot.com',
-  messagingSenderId: '233388384349',
-  appId: '1:233388384349:web:089c4df4fd9230c9076db4',
+  apiKey: "AIzaSyDXCUM2AxKD73rQIy53ArxDspVYbVDBJ68",
+  authDomain: "curling-scoreboard-dev.firebaseapp.com",
+  projectId: "curling-scoreboard-dev",
+  storageBucket: "curling-scoreboard-dev.appspot.com",
+  messagingSenderId: "233388384349",
+  appId: "1:233388384349:web:089c4df4fd9230c9076db4",
 };
 
 const prodConfig = {
-  apiKey: 'AIzaSyDKWdlQbyMrhlVd7IvzXlFGukUHeGgHZh4',
-  authDomain: 'curling-scoreboard-prod.firebaseapp.com',
-  projectId: 'curling-scoreboard-prod',
-  storageBucket: 'curling-scoreboard-prod.appspot.com',
-  messagingSenderId: '513864345938',
-  appId: '1:513864345938:web:38ea0259c2b9099002b28b',
+  apiKey: "AIzaSyDKWdlQbyMrhlVd7IvzXlFGukUHeGgHZh4",
+  authDomain: "curling-scoreboard-prod.firebaseapp.com",
+  projectId: "curling-scoreboard-prod",
+  storageBucket: "curling-scoreboard-prod.appspot.com",
+  messagingSenderId: "513864345938",
+  appId: "1:513864345938:web:38ea0259c2b9099002b28b",
 };
 
-const isProd = import.meta.env.VITE_ENV === 'prod';
+const isProd = import.meta.env.VITE_ENV === "prod";
 const app = initializeApp(isProd ? prodConfig : devConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const functions = getFunctions(app, 'us-central1');
+export const functions = getFunctions(app, "us-central1");
